1d6a08677f2e296bcacb624fe2b68d607a62bd3d
david alade <alade.a@northeastern.edu>
2023-10-06 21:50:23 -0500

    update

--- frontend/mobileapp/App.js
+++ frontend/mobileapp/App.js
@@ -1,13 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, View, Text, SafeAreaView, TextInput } from 'react-native';
+
 
 export default function App() {
-  return (
+
+  const[name , setName]= useState("");
+  const[phoneNumber , setPhone] = useState("");
+  const[town, setTown] = useState("");
+
+  return <>
+ 
     <View style={styles.container}>
-      <Text>Openp App.jstart working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Join a Club</Text>
+        <TextInput
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+          placeholder={'Name'}
+          style={styles.input}
+        />
+
+        <TextInput
+          value={phoneNumber}
+          onChange={(event) => setPhone(event.target.value)}
+          placeholder={'Phone number'}
+          style={styles.input}
+        />
+
+        <TextInput
+          value={town}
+          onChange={(event) => setTown(event.target.value)}
+          placeholder={'Town name'}
+          style={styles.input}
+        />
+
+        {/* <Button
+          title={'Submit'}
+          style={styles.input}
+          onPress={this.onLogin.bind(this)}
+        /> */}
     </View>
-  );
+
+    </>
 }
 
 const styles = StyleSheet.create({
@@ -18,3 +52,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
